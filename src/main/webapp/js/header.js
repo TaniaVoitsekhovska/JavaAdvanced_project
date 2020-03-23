09a9b992f6aa84aa4683d1a2257c67d918dfd851
TaniaVoitsekhovska <taniavoitsekhovska@gmail.com>
2020-03-23 14:29:53 +0200

    added header.js file

---
+++ src/main/webapp/js/header.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.leftmenutrigger').on('click', function (e) {
+        $('.side-nav').toggleClass("open");
+        e.preventDefault();
+    });
+});
+
+
+$("a.logout").click(function () {
+
+    $.get("logout")
+        .done(function (data, textStatus, xhr) {
+            window.location = window.origin + "/IShop/index.jsp";
+        })
+        .fail(function () {
+            alert("Can't logout");
+        });
+});
